a3c937a20bb0e615d4f246670a19a94fa04a7832
Hanif Yuli Abdillah P <hanif@widyawicara.com>
2022-06-11 21:25:48 +0700

    add preview

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template, session, redirect, url_for
+from flask import Flask, jsonify, request, render_template, session, redirect, url_for, send_file
 from flask_cors import CORS
 
 app = Flask(__name__, template_folder='site')
