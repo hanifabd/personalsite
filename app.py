66be89c7331ef766a5a23392083a63b6433ae4bf
Hanif Yuli Abdillah P <hanif@widyawicara.com>
2022-06-11 18:58:21 +0700

    update app

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify, request, render_template, session, redirect, url_for
 from flask_cors import CORS
-from functionSBD import *
 
 app = Flask(__name__, template_folder='site')
 CORS(app)
