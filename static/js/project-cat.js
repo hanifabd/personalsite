f62ff865011f24a6d3e2e229811efc91c74048c2
Hanif Yuli Abdillah P <hanif@widyawicara.com>
2022-06-11 18:26:06 +0700

    push v2

---
+++ static/js/project-cat.js
@@ -0,0 +1,22 @@
+function objectStart() {
+    intro();
+}
+window.onload = objectStart;
+
+function intro() {
+    var currentDiv = $("#box1");
+    var nextDiv, count = 1;
+    var myInterval = setInterval(function() {
+        if (count == 3) {
+            count = 1;
+            currentDiv.hide();
+            currentDiv = $("#box1");
+            currentDiv.show();
+        } else {
+            count++;
+            currentDiv.hide();
+            currentDiv = currentDiv.next();
+            currentDiv.show();
+        }
+    }, 2000);
+}
